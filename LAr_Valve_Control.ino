f20eab8cec251f97aa889efd58a5cc0ded59d216
AustinMcDonald <austin.mcdonald96@mavs.uta.edu>
2020-09-28 12:47:09 -0500

    update

--- LAr_Valve_Control.ino
+++ LAr_Valve_Control.ino
@@ -13,6 +13,9 @@ const unsigned long OneMinuet = 60000;  //the value is a number of milliseconds
 
 bool Valve_Status = false;
 
+int incomingByte = 0; // for incoming serial data
+
+
 void setup()
 {
   Serial.begin(9600);
@@ -23,26 +26,37 @@ void setup()
   digitalWrite(ENG,LOW);
 }
 
-
 void loop()
 {
   //Moto.Set_Seconds_Per_Turn(5); // set seconds per turn Default is 5
   Moto.Set_Seconds_Per_Turn(5); // this is pretty fast
   //Moto.Set_Steps_Per_Turn(400) this depends on the controller
 
-  Moto.TEST_MOTOR();
-  Arduno_time = millis();
+  incomingByte = Serial.read();
   
-  counter+=1;
+  if (incomingByte > 100)
+  {
+    Moto.TEST_MOTOR();
+    Arduno_time = millis();
+    counter+=1;
+    
+    Serial.print("Counter = ");
+    Serial.println(counter);
+    Serial.print("Time = ");
+    Serial.println(Arduno_time); 
+    Serial.print("Time = ");
+    Serial.println(Arduno_time - OneMinuet);    
+  }
+  else 
+  {
+    Serial.print("Waiting ...");
+    delay(1000);
+    
+  }
   
-//  currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
-//  if (currentMillis - startMillis >= period)  //test whether the period has elapsed
-
-  Serial.print("Counter = ");
-  Serial.println(counter);
-  Serial.print("Time = ");
-  Serial.println(Arduno_time); 
-  Serial.print("Time = ");
-  Serial.println(Arduno_time - OneMinuet);  
   
 }
+
+
+//  currentMillis = millis();  //get the current "time" (actually the number of milliseconds since the program started)
+//  if (currentMillis - startMillis >= period)  //test whether the period has elapsed
